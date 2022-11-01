9faf76127d383fb1d71354db7e0af92ef6a121e6
Ganesh <ganeshkumarjasti19@gmail.com>
2022-11-01 13:22:48 +0530

    updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,19 +1,23 @@
 import logging
 from datetime import datetime
 import os
-
+import pandas as pd
 LOG_DIR="housing_logs"
 
-CURRENT_TIME_STAMP= f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
 
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+LOG_FILE_NAME=get_log_file_name()
 
 os.makedirs(LOG_DIR,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(LOG_DIR,LOG_FILE_NAME)
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
+
+
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",
-format='[%(sctime)s] %(name)s - %(levelname)s - %(message)s',
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
 level=logging.INFO
 )
 
