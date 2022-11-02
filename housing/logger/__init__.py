74bd72aa679b7e649385b8a814bb0ab0515ad5f8
Ganesh <ganeshkumarjasti19@gmail.com>
2022-11-02 15:52:10 +0530

    updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -2,7 +2,8 @@ import logging
 from datetime import datetime
 import os
 import pandas as pd
-LOG_DIR="housing_logs"
+from housing.constant import get_current_time_stamp 
+LOG_DIR="logs"
 
 def get_log_file_name():
     return f"log_{get_current_time_stamp()}.log"
@@ -20,5 +21,22 @@ filemode="w",
 format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
 level=logging.INFO
 )
+
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns=["Time stamp","Log Level","line number","file name","function name","message"]
+    log_df.columns=columns
+    
+    log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["message"]
+
+    return log_df[["log_message"]]
+
+
+
 
 
